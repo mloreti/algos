0597f2b94f6c50b5a5f4e685d5ee560eb5f48934
Mickey Loreti <mloreti10@gmail.com>
2017-01-09 11:55:41 -0800

    Implement queue with 2 stacks

---
+++ ch3/MyQueue/my_queue.rb
@@ -0,0 +1,63 @@
+# queue implemented with 2 stack
+require '../stack/stack'
+
+class MyQueue
+  attr_accessor :new, :old
+  def initialize
+    @new = Stack.new
+    @old = Stack.new
+  end
+
+  def add(data)
+    @new.push(data)
+    nil
+  end
+
+  def shift_stacks
+    if (@old.is_empty?)
+      until (@new.is_empty?)
+        @old.push(@new.pop)
+      end
+    end
+    nil
+  end
+
+  def peek
+    self.shift_stacks
+    @old.peek
+  end
+
+  def remove
+    self.shift_stacks
+    return @old.pop.data
+  end
+
+  def to_a
+    self.shift_stacks
+    output = []
+    new_node = @new.top
+    old_node = @old.top
+
+    while old_node
+      output << old_node.data
+      old_node = old_node.next
+    end
+
+    while new_node
+      output << new_node
+      new_node = new_node.next
+    end
+
+    output.map(&:data)
+  end
+
+end
+
+q = MyQueue.new
+q.add(1)
+q.add(2)
+q.add(3)
+q.add(4)
+q.add(5)
+
+p q.to_a
